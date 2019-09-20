3b252955b8420d55cb5ff567ecb4215bd6c6c833
Lizan Zhou <lizan@tetrate.io>
2019-09-20 11:57:11 -0700

    fix push destination
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- toolchains/regenerate.sh
+++ toolchains/regenerate.sh
@@ -42,6 +42,6 @@ if [[ "true" == "${COMMIT_TOOLCHAINS}" ]]; then
   git commit -m "${COMMIT_MSG}"
 
   if [[ "${SOURCE_BRANCH}" =~ ^refs/heads/.* ]]; then
-    git push git@github.com/envoyproxy/envoy-build-tools.git "HEAD:${SOURCE_BRANCH}"
+    git push git@github.com:envoyproxy/envoy-build-tools.git "HEAD:${SOURCE_BRANCH}"
   fi
 fi
