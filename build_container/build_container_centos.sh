502b178c22916c546c15781ad48fb4f2828daf48
Lizan Zhou <lizan@tetrate.io>
2019-10-15 14:24:17 +0000

    fix sha
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- build_container/build_container_centos.sh
+++ build_container/build_container_centos.sh
@@ -15,7 +15,7 @@ ln -s /usr/bin/ninja-build /usr/bin/ninja
 # SLES 11 has older glibc than CentOS 7, so pre-built binary for it works on CentOS 7
 LLVM_VERSION=9.0.0
 LLVM_DISTRO="x86_64-linux-sles11.3"
-LLVM_SHA256SUM="7e2846ff60c181d1f27d97c23c25a2295f5730b6d88612ddd53b4cbb8177c4b9"
+LLVM_SHA256SUM="c80b5b10df191465df8cee8c273d9c46715e6f27f80fef118ad4ebb7d9f3a7d3"
 
 # httpd24 is equired by rh-git218
 echo "/opt/rh/httpd24/root/usr/lib64" > /etc/ld.so.conf.d/httpd24.conf
