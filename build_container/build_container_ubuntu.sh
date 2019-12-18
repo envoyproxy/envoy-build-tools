8818200b1c4648e6d18d643867966abd5698d9bb
Harvey Tuch <htuch@google.com>
2019-12-17 22:20:15 -0500

    build: python3.7 support in build container.
    The default Ubuntu supplied 16.05 python3.5 continues to be the default
    python3 and will be used in most places. python3.7 from deadsnakes/ppa
    is available for scripts that rely on more recent features.
    
    Signed-off-by: Harvey Tuch <htuch@google.com>

--- build_container/build_container_ubuntu.sh
+++ build_container/build_container_ubuntu.sh
@@ -39,6 +39,11 @@ apt-get update -y
 apt-get install -y --no-install-recommends docker-ce-cli wget make cmake git python python-pip python-setuptools python3 python3-pip \
   unzip bc libtool ninja-build automake zip time gdb strace tshark tcpdump patch xz-utils rsync ssh-client google-cloud-sdk
 
+# Python 3.7
+add-apt-repository -y ppa:deadsnakes/ppa
+apt-get update
+apt install -y python3.7
+
 LLVM_VERSION=9.0.0
 case $ARCH in
     'ppc64le' )
