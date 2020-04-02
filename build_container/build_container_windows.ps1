d115cca2136f2af6f4e0b9832d16306f67b9c6f5
Sunjay Bhatia <sbhatia@pivotal.io>
2020-04-02 17:22:12 -0400

    Comment out vsbuild tools and msys to test if a smaller image will not stall
    Signed-off-by: Sunjay Bhatia <sbhatia@pivotal.io>

--- build_container/build_container_windows.ps1
+++ build_container/build_container_windows.ps1
@@ -35,33 +35,33 @@ DownloadAndCheck C:\tools\bazel\bazel.exe `
 AddToPath C:\tools\bazel
 
 # VS Build Tools
-DownloadAndCheck $env:TEMP\vs_buildtools.exe `
-                 https://aka.ms/vs/16/release/vs_buildtools.exe `
-                 4383a2e9eac72248bd56a25aed63051efb37393a7af3db4726868699c7cd99e4
-echo @"
-{
-  "version": "1.0",
-  "components": [
-    "Microsoft.VisualStudio.Component.VC.CoreBuildTools",
-    "Microsoft.VisualStudio.Component.VC.Redist.14.Latest",
-    "Microsoft.VisualStudio.Component.Windows10SDK",
-    "Microsoft.VisualStudio.Component.TestTools.BuildTools",
-    "Microsoft.VisualStudio.Component.VC.ASAN",
-    "Microsoft.VisualStudio.Component.VC.CMake.Project",
-    "Microsoft.VisualStudio.Component.VC.Tools.x86.x64",
-    "Microsoft.VisualStudio.Component.Windows10SDK.18362"
-  ]
-}
-"@ > $env:TEMP\vs_buildtools_config
-cmd.exe /s /c "$env:TEMP\vs_buildtools.exe --quiet --wait --norestart --nocache --config $env:TEMP\vs_buildtools_config"
-if ($LASTEXITCODE -ne 0) {
-  echo "VS Build Tools install failed with $LASTEXITCODE"
-  exit $LASTEXITCODE
-}
-rm -Force $env:TEMP\vs_buildtools*
-AddToPath "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Tools\MSVC\14.25.28610\bin\Hostx64\x64"
-AddToPath "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin"
-AddToPath "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\IDE\CommonExtensions\Microsoft\CMake\Ninja"
+# DownloadAndCheck $env:TEMP\vs_buildtools.exe `
+#                  https://aka.ms/vs/16/release/vs_buildtools.exe `
+#                  4383a2e9eac72248bd56a25aed63051efb37393a7af3db4726868699c7cd99e4
+# echo @"
+# {
+#   "version": "1.0",
+#   "components": [
+#     "Microsoft.VisualStudio.Component.VC.CoreBuildTools",
+#     "Microsoft.VisualStudio.Component.VC.Redist.14.Latest",
+#     "Microsoft.VisualStudio.Component.Windows10SDK",
+#     "Microsoft.VisualStudio.Component.TestTools.BuildTools",
+#     "Microsoft.VisualStudio.Component.VC.ASAN",
+#     "Microsoft.VisualStudio.Component.VC.CMake.Project",
+#     "Microsoft.VisualStudio.Component.VC.Tools.x86.x64",
+#     "Microsoft.VisualStudio.Component.Windows10SDK.18362"
+#   ]
+# }
+# "@ > $env:TEMP\vs_buildtools_config
+# cmd.exe /s /c "$env:TEMP\vs_buildtools.exe --quiet --wait --norestart --nocache --config $env:TEMP\vs_buildtools_config"
+# if ($LASTEXITCODE -ne 0) {
+#   echo "VS Build Tools install failed with $LASTEXITCODE"
+#   exit $LASTEXITCODE
+# }
+# rm -Force $env:TEMP\vs_buildtools*
+# AddToPath "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Tools\MSVC\14.25.28610\bin\Hostx64\x64"
+# AddToPath "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin"
+# AddToPath "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\IDE\CommonExtensions\Microsoft\CMake\Ninja"
 
 # 7z
 DownloadAndCheck $env:TEMP\7z.msi `
@@ -72,36 +72,36 @@ rm -Force $env:TEMP\7z.msi
 AddToPath $env:ProgramFiles\7-Zip
 
 # msys2 and required packages
-DownloadAndCheck $env:TEMP\msys2.tar.xz `
-                 http://repo.msys2.org/distrib/x86_64/msys2-base-x86_64-20190524.tar.xz `
-                 168e156fa9f00d90a8445676c023c63be6e82f71487f4e2688ab5cb13b345383
-Start-Process 7z.exe -ArgumentList "x $env:TEMP\msys2.tar.xz -o$env:TEMP\msys2.tar -y" -Wait -NoNewWindow
-Start-Process 7z.exe -ArgumentList "x $env:TEMP\msys2.tar -oC:\tools -y" -Wait -NoNewWindow
-rm -Recurse -Force $env:TEMP\msys2.tar*
-AddToPath C:\tools\msys64\usr\bin
-# Don't use Start-Process as it waits for all of bash's child processes to exit
-bash.exe -c 'pacman-key --init'
-if ($LASTEXITCODE -ne 0) {
-    echo "bash.exe exited with $LASTEXITCODE"
-    exit $LASTEXITCODE
-}
-bash.exe -c 'pacman-key --populate msys2'
-if ($LASTEXITCODE -ne 0) {
-    echo "bash.exe exited with $LASTEXITCODE"
-    exit $LASTEXITCODE
-}
-Start-Process pacman.exe -ArgumentList "-Syyuu --noconfirm" -Wait -NoNewWindow
-Start-Process pacman.exe -ArgumentList "-Syuu --noconfirm" -Wait -NoNewWindow
-Start-Process pacman.exe -ArgumentList "-S --noconfirm --needed compression diffutils git patch" -Wait -NoNewWindow
-Start-Process pacman.exe -ArgumentList "-Scc --noconfirm" -Wait -NoNewWindow
-
-# Python3 (do not install via msys2, that version behaves like posix)
-DownloadAndCheck $env:TEMP\python3-installer.exe `
-                 https://www.python.org/ftp/python/3.8.2/python-3.8.2-amd64.exe `
-                 8e400e3f32cdcb746e62e0db4d3ae4cba1f927141ebc4d0d5a4006b0daee8921
-Start-Process $env:TEMP\python3-installer.exe -ArgumentList "/quiet InstallAllUsers=1 Include_launcher=0 InstallLauncherAllUsers=0" -Wait -NoNewWindow
-rm -Force $env:TEMP\python3-installer.exe
-AddToPath $env:ProgramFiles\Python38
+# DownloadAndCheck $env:TEMP\msys2.tar.xz `
+#                  http://repo.msys2.org/distrib/x86_64/msys2-base-x86_64-20190524.tar.xz `
+#                  168e156fa9f00d90a8445676c023c63be6e82f71487f4e2688ab5cb13b345383
+# Start-Process 7z.exe -ArgumentList "x $env:TEMP\msys2.tar.xz -o$env:TEMP\msys2.tar -y" -Wait -NoNewWindow
+# Start-Process 7z.exe -ArgumentList "x $env:TEMP\msys2.tar -oC:\tools -y" -Wait -NoNewWindow
+# rm -Recurse -Force $env:TEMP\msys2.tar*
+# AddToPath C:\tools\msys64\usr\bin
+# # Don't use Start-Process as it waits for all of bash's child processes to exit
+# bash.exe -c 'pacman-key --init'
+# if ($LASTEXITCODE -ne 0) {
+#     echo "bash.exe exited with $LASTEXITCODE"
+#     exit $LASTEXITCODE
+# }
+# bash.exe -c 'pacman-key --populate msys2'
+# if ($LASTEXITCODE -ne 0) {
+#     echo "bash.exe exited with $LASTEXITCODE"
+#     exit $LASTEXITCODE
+# }
+# Start-Process pacman.exe -ArgumentList "-Syyuu --noconfirm" -Wait -NoNewWindow
+# Start-Process pacman.exe -ArgumentList "-Syuu --noconfirm" -Wait -NoNewWindow
+# Start-Process pacman.exe -ArgumentList "-S --noconfirm --needed compression diffutils git patch" -Wait -NoNewWindow
+# Start-Process pacman.exe -ArgumentList "-Scc --noconfirm" -Wait -NoNewWindow
+#
+# # Python3 (do not install via msys2, that version behaves like posix)
+# DownloadAndCheck $env:TEMP\python3-installer.exe `
+#                  https://www.python.org/ftp/python/3.8.2/python-3.8.2-amd64.exe `
+#                  8e400e3f32cdcb746e62e0db4d3ae4cba1f927141ebc4d0d5a4006b0daee8921
+# Start-Process $env:TEMP\python3-installer.exe -ArgumentList "/quiet InstallAllUsers=1 Include_launcher=0 InstallLauncherAllUsers=0" -Wait -NoNewWindow
+# rm -Force $env:TEMP\python3-installer.exe
+# AddToPath $env:ProgramFiles\Python38
 
 # Clear out $env:TEMP to save space
 rm -Recurse -Force $env:TEMP\*
