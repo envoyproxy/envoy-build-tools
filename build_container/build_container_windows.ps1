2879de7b266bd07a861b843a05e40e493a71b46f
Sunjay Bhatia <sbhatia@pivotal.io>
2020-04-02 23:16:02 -0400

    Do everything except install vs build tools and final msys2 step
    Signed-off-by: Sunjay Bhatia <sbhatia@pivotal.io>

--- build_container/build_container_windows.ps1
+++ build_container/build_container_windows.ps1
@@ -5,13 +5,14 @@ function DownloadAndCheck
 {
     param([string]$to, [string]$url, [string]$sha256)
 
+    echo "Downloading $url to $to..."
     (New-Object System.Net.WebClient).DownloadFile($url, $to)
-
-    $actual = (Get-FileHash -Path $to -Algorithm SHA256).Hash;
+    $actual = (Get-FileHash -Path $to -Algorithm SHA256).Hash
     if ($actual -ne $sha256) {
-        echo "Downloading $url to $to is invalid, expected sha256: $sha256, but got: $actual";
+        echo "Download of $url to $to is invalid, expected sha256: $sha256, but got: $actual";
         exit 1
     }
+    echo "done."
 }
 
 function AddToPath
@@ -23,6 +24,25 @@ function AddToPath
     Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
     # Add to local path so subsequent commands have access to the executables they need
     $env:PATH += ";$directory"
+    echo "Added $directory to PATH"
+}
+
+function RunAndCheckError
+{
+    param([string] $exe, [string[]] $argList, [Parameter(Mandatory=$false)] $isInstaller = $false)
+
+    echo "Running '$exe $argList'..."
+    if ($isInstaller) {
+        echo "(running as Windows software installer)"
+        Start-Process $exe -ArgumentList "$argList" -Wait -NoNewWindow
+    } else {
+        &$exe $argList
+        if ($LASTEXITCODE -ne 0) {
+            echo "$exe $argList exited with code $LASTEXITCODE"
+            exit $LASTEXITCODE
+        }
+    }
+    echo "done."
 }
 
 mkdir -Force C:\tools
@@ -53,12 +73,7 @@ echo @"
   ]
 }
 "@ > $env:TEMP\vs_buildtools_config
-cmd.exe /s /c "$env:TEMP\vs_buildtools.exe --quiet --wait --norestart --nocache --config $env:TEMP\vs_buildtools_config"
-if ($LASTEXITCODE -ne 0) {
-  echo "VS Build Tools install failed with $LASTEXITCODE"
-  exit $LASTEXITCODE
-}
-rm -Force $env:TEMP\vs_buildtools*
+# RunAndCheckError "cmd.exe" $("/s", "/c", "$env:TEMP\vs_buildtools.exe --quiet --wait --norestart --nocache --config $env:TEMP\vs_buildtools_config")
 AddToPath "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Tools\MSVC\14.25.28610\bin\Hostx64\x64"
 AddToPath "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin"
 AddToPath "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\IDE\CommonExtensions\Microsoft\CMake\Ninja"
@@ -67,40 +82,30 @@ AddToPath "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common
 DownloadAndCheck $env:TEMP\7z.msi `
                  https://www.7-zip.org/a/7z1900-x64.msi `
                  a7803233eedb6a4b59b3024ccf9292a6fffb94507dc998aa67c5b745d197a5dc
-Start-Process msiexec.exe -ArgumentList "/i $env:TEMP\7z.msi /passive /norestart" -Wait -NoNewWindow
-rm -Force $env:TEMP\7z.msi
+# msiexec needs to be run as an installer with Start-Process
+RunAndCheckError "msiexec.exe" @("/i", "$env:TEMP\7z.msi", "/passive", "/norestart") $true
 AddToPath $env:ProgramFiles\7-Zip
 
 # msys2 and required packages
-# DownloadAndCheck $env:TEMP\msys2.tar.xz `
-#                  http://repo.msys2.org/distrib/x86_64/msys2-base-x86_64-20190524.tar.xz `
-#                  168e156fa9f00d90a8445676c023c63be6e82f71487f4e2688ab5cb13b345383
-# Start-Process 7z.exe -ArgumentList "x $env:TEMP\msys2.tar.xz -o$env:TEMP\msys2.tar -y" -Wait -NoNewWindow
-# Start-Process 7z.exe -ArgumentList "x $env:TEMP\msys2.tar -oC:\tools -y" -Wait -NoNewWindow
-# rm -Recurse -Force $env:TEMP\msys2.tar*
-# AddToPath C:\tools\msys64\usr\bin
-# # Don't use Start-Process as it waits for all of bash's child processes to exit
-# bash.exe -c 'pacman-key --init'
-# if ($LASTEXITCODE -ne 0) {
-#     echo "bash.exe exited with $LASTEXITCODE"
-#     exit $LASTEXITCODE
-# }
-# bash.exe -c 'pacman-key --populate msys2'
-# if ($LASTEXITCODE -ne 0) {
-#     echo "bash.exe exited with $LASTEXITCODE"
-#     exit $LASTEXITCODE
-# }
-# Start-Process pacman.exe -ArgumentList "-Syyuu --noconfirm" -Wait -NoNewWindow
-# Start-Process pacman.exe -ArgumentList "-Syuu --noconfirm" -Wait -NoNewWindow
-# Start-Process pacman.exe -ArgumentList "-S --noconfirm --needed compression diffutils git patch" -Wait -NoNewWindow
-# Start-Process pacman.exe -ArgumentList "-Scc --noconfirm" -Wait -NoNewWindow
+DownloadAndCheck $env:TEMP\msys2.tar.xz `
+                 http://repo.msys2.org/distrib/x86_64/msys2-base-x86_64-20190524.tar.xz `
+                 168e156fa9f00d90a8445676c023c63be6e82f71487f4e2688ab5cb13b345383
+RunAndCheckError "7z.exe" @("x", "$env:TEMP\msys2.tar.xz", "-o$env:TEMP\msys2.tar", "-y")
+RunAndCheckError "7z.exe" @("x", "$env:TEMP\msys2.tar", "-oC:\tools", "-y")
+AddToPath C:\tools\msys64\usr\bin
+RunAndCheckError "bash.exe" @("-c", "pacman-key --init")
+RunAndCheckError "bash.exe" @("-c", "pacman-key --populate msys2")
+RunAndCheckError "pacman.exe" @("-Syyuu", "--noconfirm")
+RunAndCheckError "pacman.exe" @("-Syuu", "--noconfirm")
+RunAndCheckError "pacman.exe" @("-S", "--noconfirm", "--needed", "diffutils", "git", "patch", "unzip", "zip")
+# pacman.exe -Scc --noconfirm
 
 # Python3 (do not install via msys2, that version behaves like posix)
 DownloadAndCheck $env:TEMP\python3-installer.exe `
                  https://www.python.org/ftp/python/3.8.2/python-3.8.2-amd64.exe `
                  8e400e3f32cdcb746e62e0db4d3ae4cba1f927141ebc4d0d5a4006b0daee8921
-Start-Process $env:TEMP\python3-installer.exe -ArgumentList "/quiet InstallAllUsers=1 Include_launcher=0 InstallLauncherAllUsers=0" -Wait -NoNewWindow
-rm -Force $env:TEMP\python3-installer.exe
+# python installer needs to be run as an installer with Start-Process
+RunAndCheckError "$env:TEMP\python3-installer.exe" @("/quiet", "InstallAllUsers=1", "Include_launcher=0", "InstallLauncherAllUsers=0") $true
 AddToPath $env:ProgramFiles\Python38
 
 # Clear out $env:TEMP to save space
