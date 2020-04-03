9f310270eb56feacfa40500491255a89f33c92c3
Sunjay Bhatia <sbhatia@pivotal.io>
2020-04-03 14:53:39 -0400

    Run docker ps -a in background

--- build_container/docker_build_windows.sh
+++ build_container/docker_build_windows.sh
@@ -4,6 +4,8 @@ set -e
 
 echo "build envoyproxy/envoy-build-windows:${CONTAINER_TAG}-amd64"
 
+while true; do docker ps -a; sleep 10; done &
+
 docker build -f Dockerfile-windows -t envoyproxy/envoy-build-windows:${CONTAINER_TAG}-amd64 .
 
 docker images -a
