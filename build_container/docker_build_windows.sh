42ecba5d257b60d2b1c31ddfa7ea55cc42acccf1
Sunjay Bhatia <sbhatia@pivotal.io>
2020-04-15 17:53:22 -0400

    Combine docker_push{,_windows}.sh into one file
    parameterized with OS_FAMILY, OS_DISTRO, and IMAGE_ARCH
    
    Signed-off-by: Sunjay Bhatia <sbhatia@pivotal.io>

--- build_container/docker_build_windows.sh
+++ build_container/docker_build_windows.sh
@@ -2,6 +2,7 @@
 
 set -e
 
-echo "build envoyproxy/envoy-build-windows:${CONTAINER_TAG}-amd64"
+# Ignore IMAGE_ARCH as amd64 is the only option
+echo "build envoyproxy/envoy-build-${OS_DISTRO}:${CONTAINER_TAG}-amd64"
 
-docker build -f Dockerfile-windows -t envoyproxy/envoy-build-windows:${CONTAINER_TAG}-amd64 .
+docker build -f "Dockerfile-${OS_DISTRO}" -t "envoyproxy/envoy-build-${OS_DISTRO}:${CONTAINER_TAG}-amd64" .
