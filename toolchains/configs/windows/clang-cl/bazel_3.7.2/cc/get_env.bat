ceab821b8a6cff299f8c55903bd6899879727cf3
envoy-build-tools(Azure Pipelines) <envoy-build-tools@users.noreply.github.com>
2021-06-09 04:09:30 +0000

    Regenerate windows toolchains from a016801f68ef05d8cc6565b3510b097a85e51cdd
      [skip ci]
      Regenerate linux toolchains from a17abfad9c6c61d4175216769299c0623a60149f
    
      [skip ci]
      python: Remove pyyaml from env (#138)
    
    any python environments should install their own pip dependencies rather than having them in the build container
    
    currently, this causes ci to pass tests, where it would fail locally, not using the build image
    
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- toolchains/configs/windows/clang-cl/bazel_3.7.2/cc/get_env.bat
+++ toolchains/configs/windows/clang-cl/bazel_3.7.2/cc/get_env.bat
@@ -1,3 +1,3 @@
 @echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\VCVARSALL.BAT" amd64  -vcvars_ver=14.28.29910 > NUL 
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\VCVARSALL.BAT" amd64  -vcvars_ver=14.29.30037 > NUL 
 echo PATH=%PATH%,INCLUDE=%INCLUDE%,LIB=%LIB%,WINDOWSSDKDIR=%WINDOWSSDKDIR% 
