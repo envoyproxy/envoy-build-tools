73337896b8662c99643d961e51e13c1f25e6add0
Lizan Zhou <lizan@tetrate.io>
2022-12-02 11:44:53 -0800

    linux: bring toolchain PPA for gdb (#186)
    Signed-off-by: Lizan Zhou <lizan@tetrate.io>

--- build_container/build_container_ubuntu.sh
+++ build_container/build_container_ubuntu.sh
@@ -12,6 +12,9 @@ apt-get install -y --no-install-recommends locales software-properties-common ap
 # set locale
 localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
 
+# gdb
+add-apt-repository -y ppa:ubuntu-toolchain-r/test
+
 # Google Cloud SDK
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" \
   | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
