b31009d7fefa2958103155c368842b8560238326
aning <aning@squareup.com>
2023-02-03 17:57:37 -0500

    DRAFT centos8
    Signed-off-by: aning <aning@squareup.com>

--- build_container/build_container_centos.sh
+++ build_container/build_container_centos.sh
@@ -4,33 +4,55 @@ set -e
 ARCH="$(uname -m)"
 # Note: rh-git218 is needed to run `git -C` in docs build process.
 if [[ "${ARCH}" == "x86_64" ]]; then
-  yum install -y centos-release-scl epel-release
+#  yum install -y centos-release-scl epel-release
+  echo "foo"
 fi
+yum-config-manager --enable ol8_codeready_builder
+
 yum update -y
-yum install -y devtoolset-9-gcc devtoolset-9-gcc-c++ devtoolset-9-binutils java-1.8.0-openjdk-headless jq rsync \
-    rh-git218 wget unzip which make cmake3 patch ninja-build devtoolset-9-libatomic-devel openssl python27 \
-    libtool autoconf tcpdump graphviz doxygen sudo glibc-static libstdc++-static
+#yum install -y devtoolset-9-gcc devtoolset-9-gcc-c++ devtoolset-9-binutils java-1.8.0-openjdk-headless jq rsync \
+#    rh-git218 wget unzip which make cmake3 patch ninja-build devtoolset-9-libatomic-devel openssl python27 \
+#    libtool autoconf tcpdump graphviz doxygen sudo glibc-static libstdc++-static
+yum install -y \
+  binutils \
+  cmake \
+  gcc \
+  git \
+  glibc-langpack-en \
+  glibc-locale-source \
+  glibc-static \
+  libstdc++-static \
+  ncurses-compat-libs \
+  ninja-build \
+  perl \
+  python3 \
+  tcpdump \
+  unzip \
+  wget \
+  xz \
+
 
 # set locale
 localedef -c -f UTF-8 -i en_US en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
-ln -s /usr/bin/cmake3 /usr/bin/cmake
+#ln -s /usr/bin/cmake3 /usr/bin/cmake
+#ln -s /usr/bin/python2 /usr/bin/python
 
 # For LLVM to pick right libstdc++
 ln -s /opt/rh/devtoolset-9/root/usr/lib/gcc/x86_64-redhat-linux/9 /usr/lib/gcc/x86_64-redhat-linux
 
 # SLES 11 has older glibc than CentOS 7, so pre-built binary for it works on CentOS 7
-LLVM_VERSION=10.0.0
+LLVM_VERSION=15.0.0
 
 case $ARCH in
     'x86_64' )
-        LLVM_DISTRO="x86_64-linux-sles11.3"
-        LLVM_SHA256SUM="a7a3c2a7aff813bb10932636a6f1612e308256a5e6b5a5655068d5c5b7f80e86"
+        LLVM_DISTRO="x86_64-linux-gnu-rhel-8.4"
+        LLVM_SHA256SUM="20b17fabc97b93791098e771adf18013c50eae2e45407f8bfa772883b6027d30"
         ;;
     'aarch64' )
         LLVM_DISTRO="aarch64-linux-gnu"
-        LLVM_SHA256SUM="c2072390dc6c8b4cc67737f487ef384148253a6a97b38030e012c4d7214b7295"
+        LLVM_SHA256SUM="527ed550784681f95ec7a1be8fbf5a24bd03d7da9bf31afb6523996f45670be3"
         ;;
 esac
 
@@ -65,7 +87,7 @@ pushd gn
 # from https://github.com/v8/v8/commit/eac21d572e92a82f5656379bc90f8ecf1ff884fc
 # (versions 9.5.164 - 9.6.152)
 git checkout 45aa842fb41d79e149b46fac8ad71728856e15b9
-python build/gen.py
+python3 build/gen.py
 ninja -C out
 mv -f out/gn /usr/local/bin/gn
 chmod +x /usr/local/bin/gn
