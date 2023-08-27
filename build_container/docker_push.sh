b3a37c1eae937070d16fc272b8a431e0d8c34b23
Ryan Northey <ryan@synca.io>
2023-08-27 19:15:03 +0100

    ci: Make configurable
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- build_container/docker_push.sh
+++ build_container/docker_push.sh
@@ -4,20 +4,19 @@
 # CI logs.
 set -e
 
-function is_azp {
-  [[ -n "${BUILD_REASON}" ]]
-}
+IMAGE_PREFIX="${IMAGE_PREFIX:-envoyproxy/envoy-build-}"
+GCR_IMAGE_PREFIX=gcr.io/envoy-ci/
+
 
 function ci_log_run() {
-  if is_azp; then
-    echo "##[group]${*}"
-  fi
-
-  "${@}"
-
-  if is_azp; then
-    echo "##[endgroup]"
-  fi
+    if [[ -n "$CI" ]]; then
+        echo "::group::${*}"
+    fi
+    "${@}"
+    echo
+    if [[ -n "$CI" ]]; then
+        echo "::endgroup::"
+    fi
 }
 
 # Enable docker experimental
@@ -25,9 +24,9 @@ export DOCKER_CLI_EXPERIMENTAL=enabled
 
 CONTAINER_SHA="$(git log -1 --pretty=format:"%H" .)"
 
-echo "Building envoyproxy/envoy-build-${OS_DISTRO}:${CONTAINER_SHA}"
-if curl -sSLf "https://index.docker.io/v1/repositories/envoyproxy/envoy-build-${OS_DISTRO}/tags/${CONTAINER_SHA}" > /dev/null; then
-  echo "envoyproxy/envoy-build-${OS_DISTRO}:${CONTAINER_SHA} exists."
+echo "Building ${IMAGE_PREFIX}${OS_DISTRO}:${CONTAINER_SHA}"
+if curl -sSLf "https://index.docker.io/v1/repositories/${IMAGE_PREFIX}${OS_DISTRO}/tags/${CONTAINER_SHA}" > /dev/null; then
+  echo "${IMAGE_PREFIX}${OS_DISTRO}:${CONTAINER_SHA} exists."
   exit 0
 fi
 
@@ -37,12 +36,12 @@ IMAGE_TAGS=()
 
 if [[ "${SOURCE_BRANCH}" == "refs/heads/main" ]]; then
     docker login -u "$DOCKERHUB_USERNAME" -p "$DOCKERHUB_PASSWORD"
-    IMAGE_TAGS+=("envoyproxy/envoy-build-${OS_DISTRO}:${CONTAINER_SHA}")
+    IMAGE_TAGS+=("${IMAGE_PREFIX}${OS_DISTRO}:${CONTAINER_SHA}")
 
     if [[ "${PUSH_GCR_IMAGE}" == "true" ]]; then
         echo ${GCP_SERVICE_ACCOUNT_KEY} | base64 --decode | gcloud auth activate-service-account --key-file=-
         gcloud auth configure-docker --quiet
-        IMAGE_TAGS+=("gcr.io/envoy-ci/${GCR_IMAGE_NAME}:${CONTAINER_SHA}")
+        IMAGE_TAGS+=("${GCR_IMAGE_PREFIX}${GCR_IMAGE_NAME}:${CONTAINER_SHA}")
     fi
 fi
 
