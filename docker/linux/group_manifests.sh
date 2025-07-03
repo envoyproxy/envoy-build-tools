0d0c1f3cc4808920eceb6bddc89b9465c2a6402f
phlax <phlax@users.noreply.github.com>
2025-07-03 21:44:21 +0100

    docker/release: Fix manifest args (/cont) (#423)
    Signed-off-by: Ryan Northey <ryan@synca.io>

--- docker/linux/group_manifests.sh
+++ docker/linux/group_manifests.sh
@@ -8,14 +8,14 @@ if [[ -z "$TAG_SHA" ]]; then
     TAG_SHA="$(git log -1 --pretty=format:"%H" .)"
 fi
 
-VARIANTS=(ci worker devtools mobile test foobar)
+VARIANTS=(ci worker devtools mobile test)
 ARCHES=(amd64 arm64)
 
-exists() {
+exists () {
     docker manifest inspect "$1" >/dev/null 2>&1
 }
 
-append_variant() {
+append_variant () {
     local repo=$1
     local variant=$2
     local arch=$3
@@ -25,7 +25,7 @@ append_variant() {
     if ! exists "$tag"; then
         return
     fi
-    echo "--append $tag"
+    echo "$tag"
 }
 
 for repo in "${IMAGE_REPOS[@]}"; do
@@ -33,7 +33,7 @@ for repo in "${IMAGE_REPOS[@]}"; do
         vflags=()
         for arch in "${ARCHES[@]}"; do
             while read -r flag tag; do
-                vflags+=("$flag" "$tag")
+                vflags+=("$tag")
             done < <(append_variant "$repo" "$variant" "$arch")
         done
         docker buildx imagetools create --tag "${repo}:${variant}-${TAG_SHA}" "${vflags[@]}"
